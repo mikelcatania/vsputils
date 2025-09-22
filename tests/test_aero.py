1c6762f0cca840943cb12486e1666ddbb9bd08a2
mikel <mikel@nourl.com>
2025-09-22 11:03:48 +0200

    feat(aero): add aerodynamic drag calculation utilities
    - Introduced `drag_polar` function to compute induced drag polynomials
      with optional parasite drag correction.
    - Added `parasite_drag` function to model parasite drag as an inverse
      power law.
    - Created unit tests for `drag_polar` and `parasite_drag` to validate
      functionality using predefined test cases.

---
+++ tests/test_aero.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import pandas as pd
+from numpy.polynomial import Polynomial
+import numpy.testing
+import unittest
+from unittest.mock import patch
+import json
+import tempfile
+import openvsp as vsp
+import vsputils.vsputils as vspu
+import vsputils.aero as vspa
+from jsonschema import ValidationError
+
+res_dir = Path(__file__).parent.joinpath('resources')
+cur_dir = Path.cwd()
+
+
+class TestDrag(unittest.TestCase):
+
+    def test_polar(self):
+
+        rnrs = vspu.load_cases(res_dir.joinpath('cases.json'))
+        p = rnrs[0].polar
+
+        cdi  = vspa.drag_polar(p)
+        cdio = vspa.drag_polar(p, with_cdo=True)
+
+        expected_cdi  = Polynomial([-2.06392658e-06, -2.17396057e-05,  2.94595394e-02])
+        expected_cdio = Polynomial([-2.01588016e-06,  1.00050586e-05,  3.47030301e-02])
+
+        numpy.testing.assert_allclose(cdi.coef, expected_cdi.coef)
+        numpy.testing.assert_allclose(cdio.coef, expected_cdio.coef)
+
+    def test_parasite(self):
+
+        vels = [20.5776, 27.4368, 34.29600000000001, 41.1552, 48.01440000000001,
+                54.8736, 61.7328, 68.59200000000001, 75.45120000000001, 82.3104]
+        cd0s = [0.02624567, 0.02521113, 0.02445641, 0.02386864, 0.02339087,
+                0.0229906 , 0.02264764, 0.02234863, 0.02208432, 0.02184803]
+
+        cd0 = vspa.parasite_drag(vels, cd0s)
+
+        ref_vels = [10, 20, 50, 100]
+        ref_cd0s = [0.02881764, 0.02629413, 0.02329413, 0.02125431]
+        numpy.testing.assert_allclose(cd0(ref_vels), ref_cd0s, rtol=3e-7)
---
+++ vsputils/aero.py
@@ -0,0 +1,32 @@
+import numpy as np
+from numpy.polynomial import Polynomial
+from numpy import ndarray
+import pandas as pd
+from spq.spq.aero import Vel
+from collections.abc import Callable
+from scipy.optimize import curve_fit
+
+def drag_polar(polar: pd.DataFrame, with_cdo: bool = False) -> Polynomial:
+
+    cdi_pol = Polynomial.fit(polar['CLiw'], polar['CDiw'], 2)
+
+    if with_cdo:
+        cdo_pol = Polynomial.fit(polar['CLiw'], polar['CDo'], 2)
+        min_cdo_cl = cdo_pol.deriv().roots()[0]
+        cdi_pol = cdi_pol + cdo_pol - cdo_pol(min_cdo_cl)
+    
+    return cdi_pol.convert()
+
+
+def parasite_drag(v: ndarray[Vel], cd0: ndarray[float]) -> Callable[[ndarray[Vel]], ndarray[float]]:
+
+    def inv_power(x, a, b):
+        return a / (x**b)
+
+    r, c = curve_fit(inv_power, v, cd0)
+
+    def inv_p(x):
+        return r[0] / (x ** r[1])
+    return inv_p
+    
+    
