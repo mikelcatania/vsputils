fe13ca9198327a1ba848f841474c135bc9691e87
mikel <mikel@nourl.com>
2025-10-07 20:22:12 +0200

    refactor(scripts): streamline mpython.sh for temporary directory usage
    - Replace hardcoded MAIN_DIR with TMPDIR for temporary operations
    - Remove redundant environment and requirements comparison steps
    - Add micromamba environment activation and deactivation
    - Simplify Python package installation process using pip
    - Ensure cleanup of temporary directory after execution

--- scripts/mpython.sh
+++ scripts/mpython.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/bash
 
-MAIN_DIR="/home/mikel/aguro/cfd/OpenVSP/python"
 OPT_DIR="/opt/OpenVSP/python"
+TMPDIR="/tmp/vstmp"
 
-# Check if an argument is provided, and override MAIN_DIR if so
-if [ "$1" ]; then
-    MAIN_DIR="$1"
-fi
+mkdir -p ${TMPDIR}
 
-echo "Differences in environment":
-diff ${MAIN_DIR}/environment.yml ${OPT_DIR}/environment.yml
-echo
+cp -r ${OPT_DIR}/python ${TMPDIR}/
 
-echo "Differences in reqs:"
-diff ${MAIN_DIR}/requirements-dev.txt ${OPT_DIR}/requirements-dev.txt
-echo
+micromamba activate vsppytools
 
-echo "Copying python directories"
-cp -rp ${OPT_DIR}/* ${MAIN_DIR}
+cd ${TMPDIR}
 
+pip install .
+
+micromamba deactivate
+
+rm -rf ${TMPDIR}
