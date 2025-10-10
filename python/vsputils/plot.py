6b2ed0be54cf354a056ef4c58d5fd483bcbd4f67
mikel <mikel@nourl.com>
2025-10-10 09:26:27 +0200

    feat(vsputils): add plot_load function for aerodynamic load visualization
    - Introduces a new `plot_load` function in `vsputils/plot.py`
    - Enables plotting of aerodynamic load distributions with customizable
      options for vortex sheets, data type, and chord-based scaling
    - Supports grouping by angle of attack for enhanced visualization

---
+++ python/vsputils/plot.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+def plot_load(load: pd.DataFrame, ax: plt.Axes, vs: list[int] | int | None = None, dtype: str = 'cl', chord_dist: bool = False, c_ref: float = 1.0) -> list[Line2D]:
+
+    if vs is None:
+        ldf = load
+    else:
+        try:
+            ldf = load[load['VortexSheet'].isin(vs)]
+        except TypeError:
+            ldf = load[load['VortexSheet'] == vs]
+
+    dist_type = {'cl': 'cl*c/cref',
+                 'cd': 'cdi*c/cref',
+                 'cm': 'cmyi*c/cref'}
+
+    lns = []
+    for a, df in ldf.groupby('aoa'):
+        if chord_dist:
+            l, = ax.plot(df['Yavg'], df[dist_type[dtype]] / df['Chord'] * c_ref, label=a)
+        else:
+            l, = ax.plot(df['Yavg'], df[dist_type[dtype]], label=a)
+        lns.append(l)
+
+    return lns
+    
